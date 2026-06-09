cff752e0d02c7f8dbb6267fa24f9ad1a242b819b
Dron Bhattacharya <dronbhattacharyadb@gmail.com>
2026-06-10 01:15:26 +0530

    fix: update CORS headers to include 'userid' and 'user-id'

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', origin);
   }
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Cookie');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Cookie, userid, user-id');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   
   if (req.method === 'OPTIONS') {
